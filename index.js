b37231ec3cbad27473d8ad65cd7eaa055f6d02d9
colyst <mehmetecck@gmail.com>
2025-09-29 17:49:40 +0200

    remove timeout message

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ async function sendMessageViaWebSocket(userId, messageText, characterId, chatId)
       setTimeout(() => {
         if (pendingResponses.has(requestId)) {
           pendingResponses.delete(requestId);
-          resolve("beynim yetmedi");
+          return;
         }
       }, 30000);
     });
