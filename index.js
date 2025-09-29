05a2b97cac1e713ef2b697995414318c147aafd0
colyst <mehmetecck@gmail.com>
2025-09-29 18:08:34 +0200

    fixed syntax error

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ async function sendMessageViaWebSocket(userId, messageText, characterId, chatId)
       setTimeout(() => {
         if (pendingResponses.has(requestId)) {
           pendingResponses.delete(requestId);
-          console.error("ignored message or response timed out", error);;
+          console.error("ignored message or response timed out");;
         }
       }, 30000);
     });
